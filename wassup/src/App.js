ec6abe53e5bcbc519c536927e6dd780fe98af19a
Kane Rogers <kane.m.rogers@gmail.com>
2016-12-07 11:50:52 +1100

    Add Background component.

--- wassup/src/App.js
+++ wassup/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { PropTypes } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Background = ({ children }) => (
+  <div className="Background">{children}</div>
+);
+
+Background.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+const App = () => (
+  <Background>
+    <h2>Welcome to Wassup</h2>
+  </Background>
+);
 
 export default App;
